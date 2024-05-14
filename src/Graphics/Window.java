923c911374e680396ff10e08aa8018d25182a337
Adsterr <adam.szulc2050@gmail.com>
2024-05-14 19:43:48 -0400

    added a better 'look and feel'

--- src/Graphics/Window.java
+++ src/Graphics/Window.java
@@ -7,6 +7,13 @@ public class Window {
         JFrame frame = new JFrame("Irregularly Dogshit Editor");
         frame.setSize(400, 360);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         frame.setVisible(true);
         return frame;
     }
--- src/Main.java
+++ src/Main.java
@@ -19,6 +19,8 @@ public class Main {
             }
         });
 
+        
+
         JPanel buttonPanel = new JPanel();
         buttonPanel.add(saveButton);
 
