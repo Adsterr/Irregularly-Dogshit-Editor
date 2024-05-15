33d1fa8f0699a144460464a921e9fc9011a95907
LaterNerds <theo.ls.raymond@icloud.com>
2024-05-14 20:31:35 -0400

    added theme

--- src/Graphics/Window.java
+++ src/Graphics/Window.java
@@ -9,7 +9,13 @@ public class Window {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+            UIManager.put("nimbusBase", new java.awt.Color(18, 30, 49));
+            UIManager.put("nimbusBlueGrey", new java.awt.Color(128, 128, 128));
+            UIManager.put("control", new java.awt.Color(30, 30, 30));
+            UIManager.put("text", new java.awt.Color(200, 200, 200));
+            UIManager.put("nimbusLightBackground", new java.awt.Color(30, 30, 30));
+            UIManager.put("nimbusFocus", new java.awt.Color(100, 100, 100));
         } catch (Exception e) {
             e.printStackTrace();
         }
--- src/Main.java
+++ src/Main.java
@@ -19,8 +19,6 @@ public class Main {
             }
         });
 
-        
-
         JPanel buttonPanel = new JPanel();
         buttonPanel.add(saveButton);
 
